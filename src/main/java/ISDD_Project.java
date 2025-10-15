72b4dbabb86ce65f84fc8bf713065d748a200611
PanPeryskop <stanislaw.gadek@o365@us.edu.pl>
2025-10-15 12:42:11 +0200

    feat: added some menu shit

--- src/main/java/ISDD_Project.java
+++ src/main/java/ISDD_Project.java
@@ -2,6 +2,7 @@ import Config.HibernateUtil;
 import Models.Client;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 import org.hibernate.HibernateException;
@@ -15,7 +16,7 @@ public class ISDD_Project {
     public static void main(String[] args) {
         System.out.println("START");
         SessionFactory sessionFactory = DBConnect();
-        Session sesion = null;
+        Session session = null;
         Transaction tr = null;
 
         try (Scanner keyboard = new Scanner(System.in)) {
@@ -27,10 +28,10 @@ public class ISDD_Project {
 
                 switch (option) {
                     case "1":
-                        sesion = sessionFactory.openSession();
-                        tr = sesion.beginTransaction();
+                        session = sessionFactory.openSession();
+                        tr = session.beginTransaction();
                         try {
-                            Query<Client> q = sesion.createQuery("FROM Client c", Client.class);
+                            Query<Client> q = session.createQuery("FROM Client c", Client.class);
                             List<Client> clients = q.getResultList();
 
                             System.out.println("====================================================================================================================");
@@ -49,17 +50,17 @@ public class ISDD_Project {
                             if (tr != null) tr.rollback();
                             System.out.print("Error");
                         } finally {
-                            if (sesion != null && sesion.isOpen()) {
-                                sesion.close();
+                            if (session != null && session.isOpen()) {
+                                session.close();
                             }
                         }
                         break;
 
                     case "2":
-                        sesion = sessionFactory.openSession();
-                        tr = sesion.beginTransaction();
+                        session = sessionFactory.openSession();
+                        tr = session.beginTransaction();
                         try {
-                            Query<Client> p = sesion.createNativeQuery("SELECT * FROM CLIENT", Client.class);
+                            Query<Client> p = session.createNativeQuery("SELECT * FROM CLIENT", Client.class);
                             List<Client> clients2 = p.getResultList();
                             for (Client c : clients2) {
                                 System.out.println(c.getMName());
@@ -71,13 +72,66 @@ public class ISDD_Project {
                             }
                             System.err.println("Error: " + e.getMessage());
                         } finally {
-                            if (sesion != null) {
-                                sesion.close();
+                            if (session.isOpen()) {
+                                session.close();
                             }
                         }
                         break;
 
+                    case "3":
+                        session = sessionFactory.openSession();
+                        tr = session.beginTransaction();
+                        try {
+                            Query<Client> p = session.createNamedQuery("Client.findAll", Client.class);
+                            List<Client> clients3 = p.getResultList();
+                            for (Client c: clients3){
+                                System.out.println(c.getMName());
+                            }
+                            tr.commit();
+                        } catch (Exception e) {
+                            if (tr != null){
+                                tr.rollback();
+                            }
+                        } finally {
+                            if (session.isOpen()){
+                                session.close();
+                            }
+                        }
+                    break;
 
+                case "4":
+                     session = sessionFactory.openSession();
+                     tr = session.beginTransaction();
+                     try {
+                         Query<Object[]> q = session.createQuery("SELECT Client.mName, Client.mPhone FROM Client", Object[].class);
+                         List<Object[]> rows = q.getResultList();
+                         for (Object[] row : rows) {
+                            System.out.println(row[0] + " " + row[1]);
+                         }
+                         tr.commit();
+                     } catch (Exception e) {
+                        if (tr != null) tr.rollback();
+                     } finally {
+                        if (session.isOpen()) session.close();
+                     }
+                     break;
+
+                    case "5":
+                        session = sessionFactory.openSession();
+                        tr = session.beginTransaction();
+                        try {
+                            System.out.println("Podaj kategorię: A, B, C ,D lub E");
+                            String category = keyboard.nextLine().toUpperCase();
+                            Query<Object[]> q = session.createQuery("SELECT c.mName, c.mcategoryMember FROM Client c WHERE c.mcategoryMember = :category", Object[].class);
+                            q.setParameter("category", category.charAt(0));
+                            List<Object[]> rows = q.getResultList();
+                            for (Object[] row : rows) System.out.println(row[0] + " " + row[1]);
+                            tr.commit();
+                        } catch (Exception e) {
+                            if (tr != null) tr.rollback();
+                        } finally {
+                            if (session.isOpen()) session.close();
+                        }
                 }
             } while (!option.equals("0"));
         } catch (HibernateException e) {
